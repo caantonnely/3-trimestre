bc299ba4a0693fd19ce0a9e971c38fad171f3668
caantonnely <142521120+caantonnely@users.noreply.github.com>
2024-10-22 11:02:37 -0300

    Add files via upload

---
+++ Downloads/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+//categoria,
+'Programação'
+//pergunta,
+'O que é JavaScript?'
+//resposta,
+'o JavaScript é uma línguagem de programação'
+
+)
+
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O que é JavaScript?'
+    //resposta,
+    'o JavaScript é uma línguagem de programação'
+    
+    )
+
+    criaCartao(
+        //categoria,
+        'Programação'
+        //pergunta,
+        'O que é JavaScript?'
+        //resposta,
+        'o JavaScript é uma línguagem de programação'
+        
+        )
