a5882c9f0e93161b568c1c2635fc02b41a2894c7
caantonnely <142521120+caantonnely@users.noreply.github.com>
2024-10-29 11:01:08 -0300

    Add files via upload

--- Downloads/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
+++ Downloads/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -1,29 +1,34 @@
 criaCartao(
 //categoria,
-'Programação'
+'Programação',
 //pergunta,
-'O que é JavaScript?'
+'O que é JavaScript?',
 //resposta,
 'o JavaScript é uma línguagem de programação'
 
 )
 
 criaCartao(
-    //categoria,
-    'Programação'
-    //pergunta,
-    'O que é JavaScript?'
-    //resposta,
-    'o JavaScript é uma línguagem de programação'
+ //categoria,
+ 'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+'o JavaScript é uma línguagem de programação'
     
-    )
+)
 
-    criaCartao(
-        //categoria,
-        'Programação'
-        //pergunta,
-        'O que é JavaScript?'
-        //resposta,
-        'o JavaScript é uma línguagem de programação'
+ criaCartao(
+ //categoria,
+'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+ 'o JavaScript é uma línguagem de programação'
         
-        )
+ )
+ criaCartao(
+    'Literatura',
+    'Qual é o nome da Saga de Alem Fardos',
+    'O nome é Ciclo Das Trevas'
+ )
