e49c11b0e796ecec0496ff9a2db193d6f66bc152
caantonnely <142521120+caantonnely@users.noreply.github.com>
2024-11-12 09:41:05 -0300

    Add files via upload

---
+++ Downloads/3-trimestre-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+//categoria,
+'Programação',
+//pergunta,
+'O que é JavaScript?',
+//resposta,
+'o JavaScript é uma línguagem de programação'
+
+)
+
+criaCartao(
+ //categoria,
+ 'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+'o JavaScript é uma línguagem de programação'
+    
+)
+
+ criaCartao(
+ //categoria,
+'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+ 'o JavaScript é uma línguagem de programação'
+        
+ )
+ criaCartao(
+    'Literatura',
+    'Qual é o nome da Saga de Camily Antonnely',
+    'O nome é Luzes do Inverno'
+ )
---
+++ Downloads/3-trimestre-main/Downloads/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+//categoria,
+'Programação',
+//pergunta,
+'O que é JavaScript?',
+//resposta,
+'o JavaScript é uma línguagem de programação'
+
+)
+
+criaCartao(
+ //categoria,
+ 'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+'o JavaScript é uma línguagem de programação'
+    
+)
+
+ criaCartao(
+ //categoria,
+'Programação',
+ //pergunta,
+ 'O que é JavaScript?',
+ //resposta,
+ 'o JavaScript é uma línguagem de programação'
+        
+ )
+ criaCartao(
+    'Literatura',
+    'Qual é o nome da Saga de Alem Fardos',
+    'O nome é Ciclo Das Trevas'
+ )
